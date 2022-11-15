a5039d7074cea8bf3bd6f25570586d6d3fe0577f
Deorren Erive <deorren@deorrens-air.attlocal.net>
2022-11-15 08:00:16 -0800

    updated everything

--- script.js
+++ script.js
@@ -1,3 +1,121 @@
 var timeCount = document.getElementById('time-count');
 var viewScore = document.getElementById('view-score');
+var divWelcome = document.querySelector('.welcome');
+var questionTitle = document.createElement('h1');
+var btnStart = document.getElementById('start-button')
+var questionDiv = document.querySelector('.question')
+var answerList1 = document.getElementById('answer1')
+var answerList2 = document.getElementById('answer2')
+var answerList3 = document.getElementById('answer3')
+var answerList4 = document.getElementById('answer4')
+
+
+var answersDiv = document.querySelectorAll('.answers');
+
+console.log(answersDiv)
+
+
+function showQuestions(question) {
+
+    function styleDisplay() {
+        questionDiv.setAttribute('style','display: flex;justify-content:center;flex-direction: column;align-items: center;height: 200px;')
+        questionTitle.setAttribute('style','background-color: green; margin-bottom: 5%;')
+        for(var i = 0; i < answersDiv.length; i++) {
+            answersDiv[i].setAttribute('style','background-color: yellow; margin: 1% 0;')
+        }
+    
+    }
+
+    for(var keys in question) {
+        questionTitle.textContent = question['question1']
+        answerList1.textContent = question['answer1']
+        answerList2.textContent = question['answer2']
+        answerList3.textContent = question['answer3']
+        answerList4.textContent = question['answer4']
+        questionDiv.append(questionTitle,answerList1,answerList2,answerList3,answerList4)
+       
+    // questionTitle.textContent = 'What HTML stands for?'
+    // questionTitle.setAttribute('style','display:flex; justify-content:center;font-size: 120%;')
+    // questionDiv.setAttribute('style','background-color:green;height: 200px;')
+    // questionDiv.appendChild(questionTitle)
+    }
+
+    styleDisplay()
+
+}
+
+
+
+var quesTionsObj = {
+    question1: 'What HTML Stand for?',
+    answer1: 'hyper markup text language',
+    answer2: 'hooper moon text language',
+    answer3: 'hangover man think loud',
+    answer4: 'hello thank man lord'
+}    
+
+var question1correct = quesTionsObj.answer1
+
+console.log(question1correct)
+
+var quesTionsObj2 = {
+    question1: 'Who owns Twitter?',
+    answer1: 'Donald Trump',
+    answer2: 'Kanye West',
+    answer3: 'Bill Gates',
+    answer4: 'Elon Musk'
+}  
+
+var question2correct = quesTionsObj2.answer4
+
+console.log(question2correct)
+
+var quesTionsObj3 = {
+    question1: 'How many states does USA have?',
+    answer1: '42',
+    answer2: '50',
+    answer3: '99',
+    answer4: '75'
+}  
+
+var question3correct = quesTionsObj3.answer2
+
+console.log(question3correct)
+
+function hideWelcome() {
+    divWelcome.setAttribute('style','display:none;')
+}
+
+function shuffleQuestions() {
+    var array = [quesTionsObj,quesTionsObj2,quesTionsObj3]
+
+    var randomNum = Math.floor(Math.random() * array.length)
+
+    return array[randomNum]
+
+}
+
+var shuffled = shuffleQuestions()
+
+    
+
+//  console.log(gameStart())
+function startGame() {
+    hideWelcome()
+    showQuestions(shuffled)
+
+
+}
+   
+
+
+btnStart.addEventListener('click', startGame)
+
+
+    
+
+
+
+
+
 
